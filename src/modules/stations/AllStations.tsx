7a32dc65a42fcdc021bef72012d6a63314653666
scriptype <scriptyper@gmail.com>
2020-10-08 03:12:51 +0300

    List first 5 stations in AllStations page

--- src/modules/stations/AllStations.tsx
+++ src/modules/stations/AllStations.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_STATIONS = gql`
+  query {
+    allStations(first: 5) {
+      id
+      name
+      available
+    }
+  }
+`;
 
 const AllStations = () => {
-  return <>All Stations</>;
+  const { loading, error, data } = useQuery(GET_STATIONS);
+  if (loading) {
+    return <>Loading your stations...</>;
+  }
+  if (error) {
+    return <>Error: {error.message}</>;
+  }
+  if (!data) {
+    return <>No stations found.</>;
+  }
+  const { allStations } = data;
+  return (
+    <div>
+      <h1>Your Stations</h1>
+      <ul>
+        {allStations.map((station: any) => (
+          <li key={station.id}>
+            {station.name}: {station.available}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default AllStations;
