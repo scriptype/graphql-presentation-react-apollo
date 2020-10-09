614f549b2951afda0dc8d729a67db3b0be06a273
scriptype <scriptyper@gmail.com>
2020-10-09 21:10:37 +0300

    Organize colors in theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,17 @@
+const colors = {
+  black: '#222222',
+  gray: '#d4d9de',
+  lightGray: '#f2f2f2',
+  white: '#ffffff',
+};
+
 export default {
-  background: '#d4d9de',
-  contentBackground: '#f2f2f2',
-  cardBackground: '#ffffff',
-  textColor: '#222222',
+  colors,
+
+  background: colors.gray,
+  contentBackground: colors.lightGray,
+  cardBackground: colors.white,
+  textColor: colors.black,
   borderRadius: '8px',
   iconSize: '16px',
 };
