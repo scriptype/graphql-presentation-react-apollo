a27c9207b199a29463598d24ec27fdaa26da4fc7
scriptype <scriptyper@gmail.com>
2020-10-09 21:12:01 +0300

    Use rem unit in Title instead of em

--- src/modules/stations/components/Title.tsx
+++ src/modules/stations/components/Title.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledTitle = styled.h1`
-  padding: 1.2em 0 1.6em;
-  font-size: 3em;
+  padding: 3.6rem 0 4.8rem;
+  font-size: 3rem;
   font-weight: bold;
   letter-spacing: calc(-1rem / 40);
 `;
