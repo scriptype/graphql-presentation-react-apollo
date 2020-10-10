12a756ae9167156d9fe5c69af2bb80f4c37edb73
scriptype <scriptyper@gmail.com>
2020-10-10 18:26:38 +0300

    Move padding-top from Title to Container

--- src/modules/stations/components/Container.tsx
+++ src/modules/stations/components/Container.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledContainer = styled.div`
+  padding-top: 3.6rem;
+
   @media (min-width: 760px) {
     width: 600px;
     margin: 0 auto;
--- src/modules/stations/components/Title.tsx
+++ src/modules/stations/components/Title.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledTitle = styled.h1`
-  padding: 3.6rem 0 4.8rem;
+  padding-bottom: 4.8rem;
   font-size: 3rem;
   font-weight: bold;
   letter-spacing: calc(-1rem / 40);
