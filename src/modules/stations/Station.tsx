ac6d4b548de645134ff7108d96a3a23af1e324a4
scriptype <scriptyper@gmail.com>
2020-10-10 04:34:21 +0300

    Show station fields in a formatted way

--- src/modules/stations/Station.tsx
+++ src/modules/stations/Station.tsx
@@ -1,13 +1,39 @@
 import React, { useMemo } from 'react';
 import { gql, useQuery } from '@apollo/client';
+import styled from 'styled-components';
 
 import Container from './components/Container';
 import Title from './components/Title';
 import LoadingOverlay from './components/LoadingOverlay';
+import Field from './components/Field';
+import { omit } from 'common/utils';
+
+const FieldsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+  column-gap: 1rem;
+  row-gap: 2rem;
+  padding: 1.25rem 1.5rem;
+  background: ${({ theme }) => theme.cardBackground};
+  border-radius: ${({ theme }) => theme.borderRadius};
+`;
+
+const StationModel = {
+  id: Number,
+  name: String,
+  status: String,
+  inuse: Date,
+  available: Boolean,
+  connected: Boolean,
+  disabled: Boolean,
+  currency: String,
+};
 
 type Props = {
   stationId: number;
 };
+
+type StationType = typeof StationModel;
 
 const Station = ({ stationId }: Props) => {
   const GET_STATION = useMemo(
@@ -30,6 +56,8 @@ const Station = ({ stationId }: Props) => {
 
   const { loading, error, data } = useQuery(GET_STATION);
 
+  const fields = omit(Object.keys(StationModel), '__typename');
+
   return (
     <Container>
       {loading && <Title>Loading...</Title>}
@@ -37,7 +65,16 @@ const Station = ({ stationId }: Props) => {
         {data && (
           <>
             <Title>{data.station.name}</Title>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
+            <FieldsContainer>
+              {fields.map((key: string) => (
+                <Field
+                  key={`station-field-${key}`}
+                  label={key}
+                  value={data.station[key]}
+                  type={StationModel[key as keyof StationType]}
+                />
+              ))}
+            </FieldsContainer>
           </>
         )}
       </LoadingOverlay>
---
+++ src/modules/stations/components/Field.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import styled from 'styled-components';
+import moment from 'moment';
+
+import { capitalize } from 'common/utils';
+
+type Props = {
+  label: string;
+  value: string | boolean | number | null;
+  type: object;
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  line-height: 1.5rem;
+`;
+
+const Label = styled.span`
+  color: ${({ theme }) => theme.colors.darkGray};
+`;
+
+const Value = styled.span``;
+
+const BooleanValue = styled.span`
+  display: inline-block;
+  position: relative;
+  width: 2.5rem;
+  height: 1.5rem;
+  border-radius: 1rem;
+  border: 1px solid currentColor;
+  font-size: 0;
+  color: ${({ value, theme }: { value: boolean; theme: any }) =>
+    !!value ? '#60ca46' : theme.colors.darkGray};
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    display: block;
+    width: 1rem;
+    height: 1rem;
+    border-radius: 100%;
+    background: currentColor;
+    left: ${({ value }: { value: boolean }) => (!!value ? '70%' : '30%')};
+  }
+`;
+
+const Field = ({ label, value, type }: Props) => {
+  return (
+    <Container>
+      <Label>{capitalize(label)}</Label>
+      {type === Boolean ? (
+        <BooleanValue value={value as boolean}>{String(value)}</BooleanValue>
+      ) : type === Date && !!value ? (
+        <Value>{moment(value as string).format('D MMM YYYY, HH:MM')}</Value>
+      ) : (
+        <Value>{value}</Value>
+      )}
+    </Container>
+  );
+};
+
+export default Field;
