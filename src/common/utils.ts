911a781f13a0da18b72c2c0a0c85c304801ad592
scriptype <scriptyper@gmail.com>
2020-10-15 19:12:53 +0300

    Fix date format
    It was showing months instead of minutes. Also display timezone

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -5,4 +5,4 @@ export const capitalize = (str: string) => {
   return str[0].toUpperCase() + str.slice(1);
 };
 
-export const DATE_FORMAT = 'D MMM YYYY, HH:MM';
+export const DATE_FORMAT = 'D MMM YYYY, HH:mm z';
